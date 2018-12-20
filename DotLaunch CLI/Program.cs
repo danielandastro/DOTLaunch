8fc1cc1efa7b6b56b0695eaecfb18a0eb006af07
Daniel Shroff <danielshroff@hotmail.com>
2018-12-20 18:39:56 +0530

    Added context awareness

---
+++ DotLaunch CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using DOTLaunch;
+namespace DotLaunch_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DOTLaunch.Program.Main();
+        }
+    }
+}
